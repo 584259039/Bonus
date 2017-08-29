2ee3c6a8949dc7123d0acbafc7fc5872b541fa6f
2 <30824166+584259039@users.noreply.github.com>
2017-08-29 15:36:24 +0700

    add num

--- src/test.java
+++ src/test.java
@@ -3,11 +3,11 @@ public class test {
 
 	public static void main(String[] args) {
 		Manager m1 = new Manager();
-		m1.setB(100000);
+		m1.setB(50000);
 		Secretary s1 = new Secretary();
-		s1.setB(100000);
+		s1.setB(30000);
 		Programmer p1 = new Programmer();
-		p1.setB(100000);
+		p1.setB(20000);
 
 	}
 
